4396c6b6077b6cc5a2de4eb7b3e266b4e1aa132c
Arthur <arthursimonin88@gmail.com>
2025-05-05 11:02:34 +0200

    feat: update tokenVerify file

--- middlewares/tokenVerify.js
+++ middlewares/tokenVerify.js
@@ -1,13 +1,22 @@
 const jwt = require("jsonwebtoken");
+const AppError = require("../utils/AppError");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) return res.sendStatus(401);
+  if (!token) {
+    return next(new AppError("Accès refusé : token manquant", 401));
+  }
 
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return next(new AppError("Token invalide ou expiré", 403));
+    }
+
+    req.user = decoded.user; // le payload contient "user": { id, email, ... }
     next();
   });
 }
