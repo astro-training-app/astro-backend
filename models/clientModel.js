56a02318b40d8d3f19f49d3258f06faa1c793173
Arthur <arthursimonin88@gmail.com>
2025-05-05 11:53:32 +0200

    fix: clientModel id name

--- models/clientModel.js
+++ models/clientModel.js
@@ -1,7 +1,7 @@
 const db = require("../db/database");
 
-exports.findByCoachId = (coachId, callback) => {
-  const sql = "SELECT * FROM clients WHERE coach_id = ?";
+exports.findByUserId = (coachId, callback) => {
+  const sql = "SELECT * FROM clients WHERE user_id = ?"; // ✅ corrigé ici
   db.all(sql, [coachId], (err, rows) => {
     callback(err, rows);
   });
