e31d9e8eff904f4129fac1a83b079eaeec90a202
Kornel-crypto <novakoka66@gmail.com>
2025-05-11 15:07:02 +0200

    feat(mensuration) add get, post and delete fonctionnality and link to front project

--- routes/mensurationsRoutes.js
+++ routes/mensurationsRoutes.js
@@ -44,4 +44,32 @@ router.post("/", (req, res) => {
   });
 });
 
+router.get("/", (req, res) => {
+  const sql = `SELECT * FROM mensurations`;
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      console.error("Erreur SQLite :", err.message);
+      return res.status(500).json({ message: "Erreur serveur" });
+    }
+
+    res.status(200).json(rows);
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  const sql = "DELETE FROM mensurations WHERE id = ?";
+
+  db.run(sql, [id], function (err) {
+    if (err) {
+      console.error("Erreur SQLite :", err.message);
+      return res.status(500).json({ message: "Erreur lors de la suppression" });
+    }
+
+    res.status(200).json({ message: "Mensuration supprimée avec succès" });
+  });
+});
+
 module.exports = router;
