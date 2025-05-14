44484cc11832829662bde29cbcff766bc37ac3f2
Arthur <arthursimonin88@gmail.com>
2025-05-14 10:54:10 +0200

    feat: Add clientById route

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -4,7 +4,10 @@ const clientController = require("../controllers/clientController");
 const authenticateToken = require("../middlewares/authMiddleware");
 
 router.get("/", authenticateToken, clientController.getClientsForUser);
+router.get("/:id", authenticateToken, clientController.getClientById);
+
 router.post("/", authenticateToken, clientController.createClient);
+
 router.put("/:id", authenticateToken, clientController.updateClient);
 
 router.delete("/:id", authenticateToken, clientController.deleteClient);
