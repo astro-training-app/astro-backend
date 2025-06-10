282d51c726a73817d531576a66edf443f5b87583
Arthur <arthursimonin88@gmail.com>
2025-06-10 13:43:57 +0200

    fix: Add getAllCoaches route

--- routes/coachRoutes.js
+++ routes/coachRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const coachController = require("../controllers/coachController");
 const authenticateToken = require("../middlewares/authMiddleware");
 
+router.get("/all", coachController.getAllCoaches);
 router.get("/", authenticateToken, coachController.getCoach);
 
 router.put("/", authenticateToken, coachController.updateCoach);
