a3cb6a78d9c748d2dc82de578f4711b595d42361
Arthur <arthursimonin88@gmail.com>
2025-06-13 08:05:37 +0200

    feat: export server listener to another file

---
+++ app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const CORS = require("cors");
+
+const coachRoutes = require("./routes/coachRoutes");
+const authRoutes = require("./routes/authRoutes");
+const clientRoutes = require("./routes/clientRoutes");
+const measurementRoutes = require("./routes/measurementRoutes");
+
+const AppError = require("./utils/AppError");
+const globalErrorHandler = require("./controllers/errorController");
+
+const app = express();
+
+app.use(CORS());
+app.use(express.json());
+
+app.use("/api/coaches", coachRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/clients", clientRoutes);
+app.use("/api/measurements", measurementRoutes);
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.use((req, res, next) => {
+  next(new AppError(`Cannot find ${req.originalUrl} on this server!`, 404));
+});
+
+// Global error handler
+app.use(globalErrorHandler);
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,39 +1,7 @@
-require("dotenv").config(); // Load environment variables from .env file
-const express = require("express");
-const app = express();
-const coachRoutes = require("./routes/coachRoutes");
-const authRoutes = require("./routes/authRoutes");
-const clientRoutes = require("./routes/clientRoutes");
-const globalErrorHandler = require("./controllers/errorController");
-const AppError = require("./utils/AppError");
-const measurementRoutes = require("./routes/measurementRoutes");
+// index.js
+const app = require("./app");
+const PORT = process.env.PORT || 3000;
 
-const CORS = require("cors");
-app.use(CORS());
-
-app.use(express.json());
-
-app.use("/api/coaches", coachRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/clients", clientRoutes);
-app.use("/api/measurements", measurementRoutes);
-
-app.get("/ping", (req, res) => {
-  res.send("pong");
-});
-
-app.use((req, res, next) => {
-  next(
-    new AppError(
-      `Impossible de trouver ${req.originalUrl} sur ce serveur!`,
-      404
-    )
-  );
-});
-
-// Ceci est un commentaire
-app.use(globalErrorHandler);
-
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
